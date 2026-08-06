ba1df9c84452bda1991a59f7cfe62f7b7e7ded38
Arpine-Davtyan <arpinedavtyan5@gmail.com>
2026-08-06 19:19:22 +0400

    Fix

--- app/invitation/rsvp/page.tsx
+++ app/invitation/rsvp/page.tsx
@@ -1,9 +1,14 @@
+import Divider from "@/components/Divider";
+import RSVPForm from "@/components/RSVPForm";
+
 const RSVP = () => {
     return (
         <section className="section">
             <div className="frame-cream">
                 <h5 className="text-gold">Kindly reply</h5>
                 <h3>Répondez</h3>
+                <Divider />
+                <RSVPForm />
             </div>
         </section>
     )
--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { SealIcon } from "@phosphor-icons/react";
 
 const Divider = () => {
   return (
     <div className="flex-center gap-2 my-2 sm:my-3">
-      <div className="w-10 h-px bg-linear-to-r from-transparent to-gold-accent"></div>
+      <div className="w-10 h-px bg-linear-to-r from-transparent to-gold-accent" />
 
       <SealIcon
         size={10}
@@ -11,7 +13,7 @@ const Divider = () => {
         className="text-gold-accent"
       />
 
-      <div className="w-10 h-px bg-linear-to-l from-transparent to-gold-accent"></div>
+      <div className="w-10 h-px bg-linear-to-l from-transparent to-gold-accent" />
     </div>
   );
 };
