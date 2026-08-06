30bac2720cc2a652006dc7dbd3b3f7432001af21
Arpine-Davtyan <arpinedavtyan5@gmail.com>
2026-08-06 19:16:36 +0400

    Test RSVP

--- app/invitation/rsvp/page.tsx
+++ app/invitation/rsvp/page.tsx
@@ -1,12 +1,9 @@
-import Divider from "@/components/Divider";
-
 const RSVP = () => {
     return (
         <section className="section">
             <div className="frame-cream">
                 <h5 className="text-gold">Kindly reply</h5>
                 <h3>Répondez</h3>
-                <Divider />
             </div>
         </section>
     )
